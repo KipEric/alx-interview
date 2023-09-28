29db8e9b97373cf3aabf6a4a98e7c3d699782a3d
KipEric <kipkoecheric97@gmail.com>
2023-09-28 13:02:50 +0300

    c4

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,44 +2,39 @@
 """Prime Game"""
 
 
-def is_prime(n):
-    """Check if a number is prime"""
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
-def isWinner(x, nums):
-    """Determine the winner of the Prime Game"""
-    if not nums:
+def isWinner(num_rounds, rounds):
+    """Determine the winner of each round of the Prime Game"""
+    if not rounds or num_rounds < 1:
         return None
 
-    wins = {'Maria': 0, 'Ben': 0}
+    max_round = max(rounds)
 
-    for n in nums:
-        if n <= 0:
+    # Create a list to check if a number is prime
+    prime_check = [True for _ in range(max(max_round + 1, 2))]
+
+    # Sieve of Eratosthenes to find all primes up to max_round
+    for i in range(2, int(pow(max_round, 0.5)) + 1):
+        if not prime_check[i]:
             continue
-        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-        if prime_count % 2 == 0:
-            wins['Ben'] += 1
-        else:
-            wins['Maria'] += 1
+        for j in range(i * i, max_round + 1, i):
+            prime_check[j] = False
 
-    if wins['Maria'] > wins['Ben']:
-        return 'Maria'
-    elif wins['Maria'] < wins['Ben']:
-        return 'Ben'
-    else:
+    prime_check[0] = prime_check[1] = False
+
+    # Count the number of primes up to each number
+    prime_counts = [0]
+    for i in range(1, len(prime_check)):
+        prime_counts.append(prime_counts[i-1] + int(prime_check[i]))
+
+    # Determine the winner of each round
+    Maria_wins = sum(prime_counts[n] % 2 for n in rounds)
+
+    if Maria_wins * 2 == num_rounds:
         return None
+    elif Maria_wins * 2 > num_rounds:
+        return "Maria"
+    else:
+        return "Ben"
 
 
 if __name__ == "__main__":
