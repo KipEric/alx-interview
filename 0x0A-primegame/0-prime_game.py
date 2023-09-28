8c3bb8f113e96835e1199311fdba0fdfc4fca27c
KipEric <kipkoecheric97@gmail.com>
2023-09-28 12:41:01 +0300

    corr1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Prime Game"""
 
-
 def is_prime(n):
     """Check if a number is prime"""
     if n <= 1:
@@ -26,6 +25,8 @@ def isWinner(x, nums):
     wins = {'Maria': 0, 'Ben': 0}
 
     for n in nums:
+        if n <= 0: 
+            continue
         prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
         if prime_count % 2 == 0:
             wins['Ben'] += 1
@@ -41,4 +42,4 @@ def isWinner(x, nums):
 
 
 if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, -1, 0])))
